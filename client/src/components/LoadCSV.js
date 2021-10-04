38bf08dc5e58358999044536196a93707a712623
illusionoff <venom12@inbox.ru>
2021-10-04 19:13:07 +0300

    works async post if first load name list CSV of  page

--- client/src/components/LoadCSV.js
+++ client/src/components/LoadCSV.js
@@ -6,16 +6,16 @@ import { useMessage } from '../hooks/message.hook';
 // import ReactDOM from 'react-dom';
 
 export const LoadCSV = () => {
-  const messageRequest = useMessage();
+  // const messageRequest = useMessage();
   const { loading, error, request, clearError } = useHttp(); //error
   // const [form, setForm] = useState({
   //   name: "", message: ""
   // });
 
-  useEffect(() => {
-    messageRequest(error);
-    clearError();
-  }, [error, messageRequest, clearError])
+  // useEffect(() => {
+  //   messageRequest(error);
+  //   clearError();
+  // }, [error, messageRequest, clearError])
 
   // const changeHandler = event => {
   //   setForm({ ...form, [event.target.name]: event.target.value });
@@ -25,7 +25,7 @@ export const LoadCSV = () => {
   const getCSV = async () => {
     try {
       const data = await request('/api/message/getcsv', 'POST');
-      messageRequest(data.message);
+      // messageRequest(data.message);
       console.log('getCSV');
       console.log('Data:', data);
     } catch (e) { }
@@ -34,15 +34,42 @@ export const LoadCSV = () => {
   const getNamesCSV = async () => {
     try {
       const data = await request('/api/message/getdircsv', 'POST');
-      messageRequest(data.message);
+      // messageRequest(data.message);
       console.log('getdircsv')
       console.log('Data:', data);
     } catch (e) { }
   }
 
   // getNamesCSV();
+  // const componentDidMountM = () => {
+  //   componentDidMount() {
+  //     fetch('http://localhost:3000/data/newsData.json')
+  //       .then(response => {
+  //         return response.json()
+  //       })
+  //       .then(data => {
+  //         console.log(this)
+  //         console.log('приехали данные ', data)
+  //       })
+  //   }
+  // }
 
-
+  const SampleComponent = () => {
+    useEffect(() => {
+      // код для запуска при монтировании компонента
+      // fetch('http://localhost:3000/data/newsData.json')
+      //   .then(response => {
+      //     return response.json()
+      //   })
+      //   .then(data => {
+      //     console.log(this)
+      //     console.log('приехали данные ', data)
+      //   })
+      getNamesCSV();
+    }, [])
+    return (<div> foo </div>)
+  }
+  SampleComponent();
 
   return (
     <>
