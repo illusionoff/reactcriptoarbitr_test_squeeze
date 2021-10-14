02054858bb5e48b30ef148b740cbb744bfc5952c
MiniPC <venom12@inbox.ru>
2021-10-14 14:19:11 +0300

    good once start function testGetdircsv()

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,22 +1,104 @@
 // import React from 'react';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // import { Line } from 'react-chartjs-2';
-import ViewChart from './ViewChart';
-import { DownloadCSV } from './DownloadCSV';
+// import ViewChart from './ViewChart';
+// import { DownloadCSV } from './DownloadCSV';
+import { postData, once } from '../functions/functions';//../hooks/message.hook
+async function testGetdircsv() {
+  console.log('startt function getdircsv()');
+  try {
+    let getdircsv = await postData('/api/message/getdircsv', {})
+      .then(response => {
+        // console.log('getdircsv запрос списка файлов в каталоге /api/message/getdircsv ');
+        // console.log('getdircsv response=', response);
+        const nameFile = response.namesFiles[response.namesFiles.length - 1];
+        // console.log('getdircsv nameFile=', nameFile);
+        return nameFile
+      })
+    let loadfile = await postData('/api/message/loadfile', { name: getdircsv }) //{ name: state.value }
+    // console.log('getdircsv data getdircsv=', getdircsv);
+    // console.log('getdircsv data loadfile=', loadfile);
+    // console.log('getdircsv 2запрос загрузки данных выбранного файла /api/message/loadfile ');
+    return loadfile
+    // updateDataFunc(data);// изменяем стейт в Chart.js
+  } catch (e) { console.log('ERROR function getdircsv', e) }
+}
+
+// let testGetdircsvOnce = once(function () {
+//   console.log('Запущено!');
+//   testGetdircsv().then((loadfile) => {
+//     console.log('END getdircsv loadfile=', loadfile);
+//   });
+// });
+
+console.log('this index.js React');
+// let getDataCsvClosureOnce = once(() => {
+//   testGetdircsv().then((loadfile) => {
+//     console.log('END getdircsv loadfile=', loadfile);
+//   })
+// }
+// );
+
+
+
 
 export const Chart = () => {
-  const [dataCsv, setDataCsv] = useState({ data: 'Данных еще нет' });
+  const hook = () => {
+    testGetdircsv().then((loadfile) => {
+      console.log('END getdircsv loadfile=', loadfile);
+    })
+  };
 
-  const updateData = (value) => {
-    setDataCsv(value)
+
+  // Tell react to run useEffect once the component is loaded
+  useEffect(hook, []); // если указать files во втором парамметре массиве то бесконечный цикл
+  // const [dataCsv, setDataCsv] = useState({ data: 'Данных еще нет' });
+
+  // const updateData = (value) => {
+  //   setDataCsv(value)
+  // }
+  // return (
+  //   <>
+  //     <div><h3 className="page-title white-text">Write to me</h3></div>
+  //     <DownloadCSV updateData={updateData} />
+  //     <ViewChart ViewChart={dataCsv} />
+
+  //   </>
+  // )
+  function changeHandler() {
+    console.log('this changeHandler');
+  }
+  function writeMeHandler() {
+    console.log('this writeMeHandler button');
   }
   return (
     <>
-      <div><h3 className="page-title white-text">Write to me</h3></div>
-      <DownloadCSV updateData={updateData} />
-      <ViewChart ViewChart={dataCsv} />
-
+      <input
+        placeholder="Введите ваше имя"
+        id="name"
+        type="text"
+        name="name"
+        className="yellow-input validate"
+        minLength="3"
+        // // pattern=".{3,500}"
+        maxLength="30"
+        data-length="30"
+        required
+        value='text'//{form.name}
+        onChange={changeHandler}
+      // onChange={changeHandler}
+      />
+      <div className="card-action">
+        <button
+          // className="btn yellow darken-4"
+          className="btn cyan darken-1"
+          onClick={writeMeHandler}
+        // disabled={loading}
+        >
+          Отправить
+        </button>
+      </div>
     </>
   )
 }
--- routes/routerMessage.js
+++ routes/routerMessage.js
@@ -101,76 +101,76 @@ const processFile = async (CSVFilePath) => {
   return dataObj//records
 }
 
-router.post('/getcsv',
-  [
-    // // check('name', 'Вы ввели имя меньше двух символов или более 30 символов')
-    // body('name')
-    //   // .notEmpty()
-    //   // .withMessage("поле пустое")
-    //   .isLength({ min: 3, max: 30 }) // .isEmpty() работает наоборот если не пусто то вызывает ошибку
-    //   .withMessage("Вы ввели имя меньше трех символов или более 30 символов")
-    //   .custom((value) => {
-    //     // let regexp = /^[a-z0-9_-]{3,16}$/; // проверка
-    //     // const regexp = /^([а-яё]+|[a-z0-9_-]){3,16}$/; // проверка  на name русские и латинские символы
-    //     // const regexp = /^([а-яё]+|[a-z0-9_-]){3,30}$/; // проверка  на name русские и латинские символы
-    //     // const regexp = /^([а-яё]+|[a-zA-Z-9_-]){3,30}$/; // проверка  на name русские и латинские символы
-    //     const regexp = /^([а-яА-ЯёЁa-zA-Z0-9]){3,30}$/; // проверка  на name русские и латинские символы
-    //     return regexp.test(value); // возвращает true либо false
-    //   })
-    //   .withMessage("Недопустимые символы в строке имени")
-    //   .trim()
-    //   .escape(),
-    // check('message', 'Минимальная длина сообщения 10 символов, а максимальная 1000')
-    //   .isLength({ min: 10, max: 1000 })
-  ], async function (req, res) {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({
-          errors: errors.array(),
-          // message: 'Некорректный данные при регистрации'
-          message: errors.array()[0].msg
-        })
-      }
+// router.post('/getcsv',
+//   [
+//     // // check('name', 'Вы ввели имя меньше двух символов или более 30 символов')
+//     // body('name')
+//     //   // .notEmpty()
+//     //   // .withMessage("поле пустое")
+//     //   .isLength({ min: 3, max: 30 }) // .isEmpty() работает наоборот если не пусто то вызывает ошибку
+//     //   .withMessage("Вы ввели имя меньше трех символов или более 30 символов")
+//     //   .custom((value) => {
+//     //     // let regexp = /^[a-z0-9_-]{3,16}$/; // проверка
+//     //     // const regexp = /^([а-яё]+|[a-z0-9_-]){3,16}$/; // проверка  на name русские и латинские символы
+//     //     // const regexp = /^([а-яё]+|[a-z0-9_-]){3,30}$/; // проверка  на name русские и латинские символы
+//     //     // const regexp = /^([а-яё]+|[a-zA-Z-9_-]){3,30}$/; // проверка  на name русские и латинские символы
+//     //     const regexp = /^([а-яА-ЯёЁa-zA-Z0-9]){3,30}$/; // проверка  на name русские и латинские символы
+//     //     return regexp.test(value); // возвращает true либо false
+//     //   })
+//     //   .withMessage("Недопустимые символы в строке имени")
+//     //   .trim()
+//     //   .escape(),
+//     // check('message', 'Минимальная длина сообщения 10 символов, а максимальная 1000')
+//     //   .isLength({ min: 10, max: 1000 })
+//   ], async function (req, res) {
+//     try {
+//       const errors = validationResult(req);
+//       if (!errors.isEmpty()) {
+//         return res.status(400).json({
+//           errors: errors.array(),
+//           // message: 'Некорректный данные при регистрации'
+//           message: errors.array()[0].msg
+//         })
+//       }
 
-      const { name } = req.body
-      let { message } = req.body
-      console.log('routerMessage name', name);
-      console.log('routerMessage name', message);
-      // !!! использовал раньше "res.json(await quotes.getMultiple(name, message))" что вызывало ошибку, потому что уже res дали ответ и пытаюсь получается повторно отсылаю ответ браузеру
+//       const { name } = req.body
+//       let { message } = req.body
+//       console.log('routerMessage name', name);
+//       console.log('routerMessage name', message);
+//       // !!! использовал раньше "res.json(await quotes.getMultiple(name, message))" что вызывало ошибку, потому что уже res дали ответ и пытаюсь получается повторно отсылаю ответ браузеру
 
-      // List files working true
-      // const testFolder = './tests/';./testCSV/
-      //////////////////
-      // const testFolder = './testCSV/';
-      // fs.readdir(testFolder, (err, files) => {
-      //   files.forEach(file => {
-      //     console.log(file);
-      //   });
-      // });
-      //
+//       // List files working true
+//       // const testFolder = './tests/';./testCSV/
+//       //////////////////
+//       // const testFolder = './testCSV/';
+//       // fs.readdir(testFolder, (err, files) => {
+//       //   files.forEach(file => {
+//       //     console.log(file);
+//       //   });
+//       // });
+//       //
 
-      // processFile("./testCSV/test2_profit_651_1631860141152.csv")
-      processFile(`${FILES_FOLDER}1631860141152_651_test2_profit.csv`)
-        // .then((result2) => { console.log('result2=', result2) })
-        // .then((dataObj) => { res.status(201).json({ message: dataObj }) })
-        // .then((dataObj) => { console.log('dataObj=', dataObj) })
-        .then((dataObj) => {
-          console.log('dataObj=', dataObj);
-          // res.status(201).json({ message: 'download CSV file' });
-          res.status(201).json({ message: dataObj });
-        })
-        .catch(console.error)
+//       // processFile("./testCSV/test2_profit_651_1631860141152.csv")
+//       processFile(`${FILES_FOLDER}1631860141152_651_test2_profit.csv`)
+//         // .then((result2) => { console.log('result2=', result2) })
+//         // .then((dataObj) => { res.status(201).json({ message: dataObj }) })
+//         // .then((dataObj) => { console.log('dataObj=', dataObj) })
+//         .then((dataObj) => {
+//           console.log('dataObj=', dataObj);
+//           // res.status(201).json({ message: 'download CSV file' });
+//           res.status(201).json({ message: dataObj });
+//         })
+//         .catch(console.error)
 
-      // res.status(201).json({ message: 'download CSV file' });
+//       // res.status(201).json({ message: 'download CSV file' });
 
-    } catch (err) {
-      // Упростить вывод ошибок при подготовке  prodaction ( лишняя информация для пользователя)
-      console.error(`Error while getting quotes `, err.message);
-      // res.status(err.statusCode || 500).json({ 'message': err.message || 'Что-то пошло не так, попробуйте снова' });
-      res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' });
-    }
-  });
+//     } catch (err) {
+//       // Упростить вывод ошибок при подготовке  prodaction ( лишняя информация для пользователя)
+//       console.error(`Error while getting quotes `, err.message);
+//       // res.status(err.statusCode || 500).json({ 'message': err.message || 'Что-то пошло не так, попробуйте снова' });
+//       res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' });
+//     }
+//   });
 
 router.post('/getdircsv',
   [], async function (req, res) {
@@ -189,7 +189,7 @@ router.post('/getdircsv',
           namesFiles.push(file);
           console.log(file);
         });
-        res.status(201).json({ namesfiles: namesFiles });
+        res.status(201).json({ namesFiles });
 
       });
       //
