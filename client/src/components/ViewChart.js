5bd8d246c154d2d4b57f634735042f0aa749b5b9
MiniPC <venom12@inbox.ru>
2021-11-04 16:41:50 +0300

    css

--- client/src/components/ViewChart.js
+++ client/src/components/ViewChart.js
@@ -313,11 +313,10 @@ function ViewChart(props) {
   return (
     <>
       <div >
-        <h3 className="page-title white-text">About</h3>
+        <h3 className="page-title white-text">Графики не загружены!</h3>
         <div className="container">
           <blockquote>
-            Я начинающий Back-end, Full Stack Nodejs разработчик, которому интересно создавать новое, пробираться через ошибки и проблемы, постоянно обучаться.
-            Чувствую себя наполненным восторгом и эйфорией, как ребенок, видя результат своих творений, осознавая полезность своих способностей. dsds
+            Графики не загружены!
         </blockquote>
         </div >
       </div>
