2951faf1e3766673aff5bb2f6449f3a26c3d9bfc
MiniPC <venom12@inbox.ru>
2021-11-04 11:21:32 +0300

    return getDataFileMemo()

--- client/src/components/DownloadCSV.js
+++ client/src/components/DownloadCSV.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef, useMemo } from 'react';
-import { getDataFileMemo, getDataFile } from '../functions/functions';
+import React, { useState, useEffect, useRef } from 'react';
+import { getDataFileMemo } from '../functions/functions';
 
 function ListSelect(props) {
   return <option value={props.number}>{props.value}</option>
@@ -12,60 +12,12 @@ function listItemsFun(propsNameFilesSelect, listItems) {
   return listItems
 }
 
-// function useMemoCompare(next, compare) {
-//   // ref для хранения предыдущего значения
-//   const prevRef = useRef();
-//   const prev = prevRef.current;
-
-//   // передаем предыдущее и новое значения в функцию
-//   // для определения их идентичности
-//   const isEqual = compare(prev, next);
-
-//   // если значения не равны, обновляем prevRef
-//   // обновление осуществляется только в случае неравенства значений
-//   // поэтому, если функция вернула true, хук возвращает старое значение
-//   useEffect(() => {
-//     if (!isEqual) {
-//       prevRef.current = next;
-//     }
-//   });
-
-//   // если значения равны, возвращаем старое значение
-//   return isEqual ? prev : next;
-// }
-
-
 export const DownloadCSV = (props) => {
   let firstValueSelected = props.nameFilesSelect[0];
   console.log(' firstValueSelected DownloadCSV=', firstValueSelected);
 
   const [nameFile, setNameFile] = useState(firstValueSelected);
   const listItems = useRef([]);
-  const getFileNameMemo = useMemo(() => {
-    console.log('getFileNameMemo!!!! DownloadCSV')
-    return getDataFile(nameFile)
-  }, [nameFile]);
-  // const objFinal = useMemoCompare(obj, (prev, next) => {
-  //   return prev && prev === next;
-  // });
-  // const returnObjReference = useMemo((newObj) => {
-  //   const obj ={};
-  //   obj.dataChart=newObj;
-  //   return obj
-  // }, [newObj]);
-
-  // const getFileNameMemo = useMemo(() => {
-  //   // objFinal
-  //   const resultGetDataFile= getDataFile(nameFile);
-
-  //   return returnObjReference(newObj)
-  // }, [nameFile]);
-
-  // useEffect(() => {
-  //   // вызываем метод объекта и присваиваем результат состоянию
-  //   return objFinal.someMethod().then((value) => setState(value));
-  // }, [objFinal]);
-
 
   const firstNameFileHook = () => {
     setNameFile(firstValueSelected);
@@ -76,18 +28,13 @@ export const DownloadCSV = (props) => {
   useEffect(firstNameFileHook, [firstValueSelected, props.nameFilesSelect]);
 
   const handleSubmit = (event) => {
-    // getDataFileMemo(nameFile)
-    //   .then((data) => {
-    //     console.log(data);
-    //     console.log('запрос загрузки данных выбранного файла /api/message/loadfile ');
-    //     props.updateData(data);
-    //   });
-
-    // getFileNameMemo(nameFile);
-    // props.updateData(getFileNameMemo);
-
-
-    event.preventDefault();// обязательно должно быть  если не вставить, то перезагружается вся страница
+    getDataFileMemo(nameFile)
+      .then((data) => {
+        console.log(data);
+        console.log('запрос загрузки данных выбранного файла /api/message/loadfile ');
+        props.updateData(data);
+      });
+    event.preventDefault();// обязательно должно быть  если не вставить, то перезагружается вся страница.
   }
 
   const handleChange = (event) => {
